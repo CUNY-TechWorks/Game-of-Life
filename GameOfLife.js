070d1c02fd3a459088e5cc9a005449687b3e12f7
unknown <matthewpiedra1228@gmail.com>
2020-06-20 17:29:06 -0400

    My GameOfLife

---
+++ GameOfLife.js
@@ -0,0 +1,114 @@
+class GameOfLife {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.board = this.makeBoard();
+  }
+  /**
+   * Returns a 2D Array
+   */
+  makeBoard() {
+    // TODO: Create and return an 2D Array
+    // with `this.height` as rows and `this.width` as cols.
+    // For example, given a height of 4 and a width of 3, it will generate:
+    // [
+    //  [0, 0, 0],
+    //  [0, 0, 0],
+    //  [0, 0, 0],
+    //  [0, 0, 0],
+    // ]
+    let array = [];
+    for(let r=0;r<this.height;r++) {
+      let innerArr = [];
+      for(let c=0;c<this.width;c++) {
+          innerArr.push(0)
+       }
+       array.push(innerArr);
+    }
+    return array;
+  }
+  
+  getCell(row,col) {
+    let board = this.board;
+    if(row < 0 || col < 0) {
+      return 'dead';
+    }
+    else if(row > this.height || col > this.width) {
+      return 'dead'; 
+    }
+    return board[row][col];
+  }
+
+  setCell(value,row,col) {
+     let board = this.board;
+     if(row < 0 || col < 0) {
+       return 'illegal cord';
+     }
+     else if(row > this.height || col > this.width) {
+       return 'illegal cord';
+     }
+     board[row][col] = value;
+  }
+  
+  toggleCell(row,col) {
+     let board = this.board;
+     if(board[row][col] === 0) {
+        board[row][col] = 1;
+     }
+     else {
+        board[row][col] = 0;
+     }
+  }
+  
+  livingNeighbors(row, col) {
+    // array of all possible neighbors  
+    let array = [[row-1,col-1],[row-1,col],[row+1,col],[row-1,col+1],[row,col+1],[row,col-1],[row+1,col-1],[row+1,col+1]];
+    
+    let board = this.board
+    
+    let count = 0;
+    for(let i=0;i<array.length;i++) {
+      if((array[i][0] >= 0 && array[i][1] >= 0) && (array[i][0] < this.height && array[i][1] < this.width)) {
+        if(board[array[i][0]][array[i][1]] !== 0) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Given the present board, apply the rules to generate a new board
+   */
+  tick() {
+    // TODO: Here is where you want to loop through all the cells
+    // on the existing board and determine, based on it's neighbors,
+    // whether the cell should be dead or alive in the new board
+    // (the next iteration of the game)
+    //
+    // You need to:
+    // 1. Count alive neighbors for all cells
+    // 2. Set the next state of all cells in newBoard,
+    // based on their current alive neighbors
+    const newBoard = this.makeBoard();
+
+    for(let r=0;r<this.height;r++) {
+      for(let c=0;c<this.width;c++) {
+         if(this.livingNeighbors(r,c) < 2 && this.getCell(r,c) === 1) { // dies
+            newBoard[r][c] = 0;
+         }
+         if((this.livingNeighbors(r,c) === 2 || this.livingNeighbors(r,c) === 3) && (this.getCell(r,c) === 1)) { // survives
+            newBoard[r][c] = 1;
+         }
+         if(this.livingNeighbors(r,c) > 3 && this.getCell(r,c) === 1) { // dies
+            newBoard[r][c] = 0;
+         }
+         if(this.livingNeighbors(r,c) === 3 && this.getCell(r,c) === 0) { // reproduces/comes back to life
+            newBoard[r][c] = 1;
+         }
+      }
+   }
+
+    this.board = newBoard;
+  }
+}
---
+++ main.js
@@ -0,0 +1,132 @@
+const width = 25;
+const height = 20; // width and height dimensions of the board
+
+/**
+ * Create a Game of Life instance
+ */
+
+const gol = new GameOfLife(width, height);
+
+/**
+ * create a table and append to the DOM
+ */
+
+// Actual table cells
+const tds = [];
+
+// <table> element
+const table = document.createElement("tbody");
+// build a table row <tr>
+for (let h = 0; h < height; h++) {
+  const tr = document.createElement("tr");
+  // build a table column <td>
+  for (let w = 0; w < width; w++) {
+    const td = document.createElement("td");
+    // We'll put the coordinates on the cell
+    // Element itself (using dataset),
+    // letting us fetch it in a click listener later.
+    td.dataset.row = h;
+    td.dataset.col = w;
+    tds.push(td);
+    tr.append(td);
+  }
+  table.append(tr);
+}
+document.getElementById("board").append(table);
+/**
+ * Draws every cell from the gol instance into an actual, visible DOM element
+ */
+
+const paint = () => {
+  // TODO:
+  //   1. For each <td> in the table:
+  //     a. If its corresponding cell in gol instance is alive,
+  //        give the <td> the `alive` CSS class.
+  //     b. Otherwise, remove the `alive` class.
+  //
+  // To find all the <td>s in the table, you might query the DOM for them, or you
+  // could choose to collect them when we create them in createTable.
+  // 
+  // HINT:
+  //   https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  //   https://developer.mozilla.org/en-US/docs/Web/API/Element/getElementsByTagName
+let cells = document.getElementsByTagName("td");
+for(let cell of cells) {
+   let row = cell.getAttribute('data-row');
+   let col = cell.getAttribute('data-col');
+   if(gol.board[row][col] === 1) {
+     cell.classList.toggle("alive",true);
+   }  
+   else { 
+     cell.classList.toggle("alive",false);
+   }
+  } 
+}
+
+/**
+ * Event Listeners
+ */
+
+document.getElementById("board").addEventListener("click", event => {
+  // TODO: Toggle clicked cell (event.target) and paint
+   let index = tds.indexOf(event.target); 
+
+   let row = tds[index].getAttribute('data-row');
+   let col = tds[index].getAttribute('data-col');
+
+   gol.toggleCell(row,col);
+
+   paint();
+});
+
+document.getElementById("step_btn").addEventListener("click", event => {
+  // TODO: Do one gol tick and paint
+   gol.tick();
+  
+   paint();
+});
+
+let intervalID = false;
+document.getElementById("play_btn").addEventListener("click", event => {
+  // TODO: Start playing by calling `tick` and paint
+  // repeatedly every fixed time interval.
+  // HINT:
+  // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+  
+  let enableAutoPlay = () => {
+    gol.tick();  
+
+    paint();
+  }
+
+  intervalID = setInterval(enableAutoPlay,300);
+});
+
+document.getElementById("random_btn").addEventListener("click", event => {
+  // TODO: Randomize the board and paint
+  let board = gol.board;
+  for(let r=0;r<height;r++) {
+    for(let c=0;c<width;c++) {
+       board[r][c] = Math.floor(Math.random() * 2);
+    }
+  }
+  
+  paint();
+});
+
+document.getElementById("clear_btn").addEventListener("click", event => {
+  // TODO: Clear the board and paint
+   let board = gol.board;
+    
+   for(let r=0;r<height;r++) {
+     for(let c=0;c<width;c++) {
+        board[r][c] = 0;
+     }
+   }
+   
+   paint();
+});
+
+document.getElementById("pause_btn").addEventListener("click", event => { 
+   clearInterval(intervalID);
+});
